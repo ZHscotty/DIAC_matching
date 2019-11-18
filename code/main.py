2aa4e46054c328ae75057b8fdf188e36e069d396
ZHscotty <z1zhaohang@163.com>
2019-11-18 16:30:50 +0800

    new word2vec

--- code/main.py
+++ code/main.py
@@ -7,14 +7,19 @@ import numpy as np
 path_trainset = '../data/trainset.txt'
 path_testset = '../data/testset.txt'
 path_devset = '../data/devset.txt'
+path_word2index = '../data/word2index.txt'
+path_w2vmodel = '../result/model/w2v/word2vec_model'
+path_w2v = '../data/w2v'
 #
+embedding_matrix = utils.get_embeddingmatrix(path_word2index, path_w2vmodel, path_w2v)
 m = Model()
 m.train(path_trainset, path_devset)
 result = m.predict(path_testset)
 id = utils.get_testid(path_testset)
-output = [result, id]
+output = [id, result]
 output = np.array(output)
 output = np.transpose(output)
 print('output shape', output.shape)
 d = pd.DataFrame(output)
-d.to_csv('../result/submit/result.csv', encoding='utf-8', index=False, header=False)
+d.to_csv('../result/submit/result.csv', encoding='utf-8', index=False, header=False, sep='\t')
+
