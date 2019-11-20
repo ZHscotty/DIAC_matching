c4f578413adec8bce4335213cf19e0d6f8f009cb
ZHscotty <z1zhaohang@163.com>
2019-11-20 15:30:35 +0800

    update

--- code/utils.py
+++ code/utils.py
@@ -1,10 +1,13 @@
 import jieba
 import re
 import ast
+import os
 import numpy as np
 import gensim
+from sklearn.model_selection import KFold
 
 
+# 获取Word2index
 def get_word2index(path):
     word2index = {}
     with open(path, 'r', encoding='utf-8') as f:
@@ -14,12 +17,14 @@ def get_word2index(path):
     return word2index
 
 
+# 加载停用词表
 def get_stopword(path):
     with open(path, 'r', encoding='utf-8') as f:
         stopword = [x.strip() for x in f.readlines()]
     return stopword
 
 
+# 分词
 def tokenize(input, path_stopword):
     # input-str
     # path_stopword - stopword.txt
@@ -38,6 +43,7 @@ def tokenize(input, path_stopword):
     return words
 
 
+# 获取words级别的最大长度
 def get_maxlen(path):
     words = []
     with open(path, 'r', encoding='utf-8') as f:
@@ -51,17 +57,21 @@ def get_maxlen(path):
 
     return max(lent), sum(lent)/len(lent)
 
+
+# 获取测试集的id信息
 def get_testid(path):
     with open(path, 'r', encoding='utf-8') as f:
         ids = [x.strip().split('\t')[0] for x in f.readlines()]
     return ids
 
 
+# 获取训练集的总数
 def get_trainnums(path):
     with open(path, 'r', encoding='utf-8') as f:
         return len(f.readlines())
 
 
+# 获取预训练词向量
 def get_embeddingmatrix(path_word2index, path_w2vmodel, path_w2v):
     word2index = get_word2index(path_word2index)
     model = gensim.models.word2vec.Word2Vec.load(path_w2vmodel)
@@ -72,6 +82,26 @@ def get_embeddingmatrix(path_word2index, path_w2vmodel, path_w2v):
             emd[int(word2index[x])] = matrix[word2index[x]]
     print('emd_matrix shape:', emd.shape)
     return matrix
+
+
+# k-fold
+def kfold(train_dir, k):
+    os.makedirs(path=os.path.join(train_dir, '/{}_fold'.format(k)))
+    with open(os.path.join(train_dir, 'train_data.txt'), 'r', encoding='utf-8') as f:
+        train_data = [line for line in f.readlines()]
+    kfold = KFold(n_splits=k, shuffle=True, random_state=1)
+
+    for index in range(k):
+        f1 = open(os.path.join(os.path.join(train_dir, '/{}_fold'.format(k)), '/train{}'.format(index)))
+        f2 = open(os.path.join(os.path.join(train_dir, '/{}_fold'.format(k)), '/dev{}'.format(index)))
+        for train, test in kfold.split(train_data):
+            for x in train_data[train]:
+                f1.write(x)
+            for x_ in train_data[test]:
+                f2.write(x_)
+        f1.close()
+        f2.close()
+
 
 
 # if __name__ == '__main__':
